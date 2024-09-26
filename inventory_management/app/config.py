9cc07100bac74bc173fdfeefbc8ed6185d7bfe1f
Hashi1995 <s223495239@deakin.edu.au>
2024-09-26 13:40:02 +1000

    first commit

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sample11_user:czw9mtCtgnOi5vb7g7LnwI5re4Ffjtzm@dpg-crqbbtg8fa8c738vkf60-a.oregon-postgres.render.com/sample11')
